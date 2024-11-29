5ff4a25431094cd3f29042dd8eb01807f2fccf9b
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-11-30 00:31:31 +0530

    first commit

---
+++ src/utils/MockQuizData.jsx
@@ -0,0 +1,19 @@
+const mockQuizData = [
+  {
+    id: 1,
+    title: "General Knowledge Quiz",
+    description: "Test your general knowledge with this quiz.",
+  },
+  {
+    id: 2,
+    title: "Science Quiz",
+    description: "Explore scientific concepts and facts.",
+  },
+  {
+    id: 3,
+    title: "History Quiz",
+    description: "Delve into the past with this history quiz.",
+  },
+];
+
+export default mockQuizData;
